24da6fa068ee2f8f50a039e5ff12cf1faa7b213c
MSeries-kelvin <aramiyorogheneserome@gmail.com>
2024-09-07 15:59:04 +0100

    projects updated - first project add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 app.post('/api/message/', (request, response) => {
     const data = request.body;
     console.log(data);
+
+    response.status(200).send({msg: "ok"});
 });
 
 app.listen('3000', () => {
